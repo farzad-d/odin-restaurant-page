b2bb7cca5cb3920d03b90949a7d43e4fc86b13f2
farzad-d <fzddnp@hotmail.com>
2025-09-25 13:52:03 +0330

    Add listeners and handlers for navbar buttons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import loadHome from "./home.js";
 import loadAbout from "./about.js";
 import loadMenu from "./menu.js";
 
-// loadHome();
-// loadAbout();
-loadMenu();
+const content = document.getElementById("content");
+
+const homeTab = document.getElementById("home-btn");
+homeTab.addEventListener("click", () => {
+  content.replaceChildren();
+  loadHome();
+});
+
+const menuTab = document.getElementById("menu-btn");
+menuTab.addEventListener("click", () => {
+  content.replaceChildren();
+  loadMenu();
+});
+
+const aboutTab = document.getElementById("about-btn");
+aboutTab.addEventListener("click", () => {
+  content.replaceChildren();
+  loadAbout();
+});
