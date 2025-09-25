0adc6541d95211925d49f75b3e656694973bc209
farzad-d <fzddnp@hotmail.com>
2025-09-25 13:26:04 +0330

    Add photo for food menu items

--- src/menu.js
+++ src/menu.js
@@ -1,51 +1,68 @@
-// import fastFoodImg from "./fast-food.png";
+import burger from "./burger.jpg";
+import sandwich from "./sandwich.jpg";
+import pasta from "./pasta.jpg";
+import pizza from "./pizza.jpg";
+import taco from "./taco.jpg";
+import salad from "./salad.jpg";
 
 const foods = [
   {
     name: "Pizza",
     price: "15",
     ingredients: "Pepperoni, Mozzarella, Tomato Sauce",
+    photoPath: pizza,
   },
   {
     name: "Burger",
     price: "8",
     ingredients: "Ground Beef, Lettuce, Tomato, Onion, Pickles, Bun",
+    photoPath: burger,
   },
   {
     name: "Taco",
     price: "3",
     ingredients: "Ground Beef, Lettuce, Cheese, Salsa, Tortilla",
+    photoPath: taco,
   },
   {
     name: "Pasta",
     price: "12",
     ingredients: "Noodles, Marinara Sauce, Meatballs",
+    photoPath: pasta,
   },
   {
     name: "Salad",
     price: "7",
     ingredients: "Lettuce, Cucumber, Tomato, Onion, Vinaigrette Dressing",
+    photoPath: salad,
   },
   {
     name: "Sandwich",
     price: "6",
     ingredients: "Turkey, Swiss Cheese, Lettuce, Mayo, Wheat Bread",
+    photoPath: sandwich,
   },
 ];
 
 class Food {
   id = crypto.randomUUID();
 
-  constructor({ name, price, ingredients }) {
+  constructor({ name, price, ingredients, photoPath }) {
     this.name = name;
     this.price = price;
     this.ingredients = ingredients;
+    this.photoPath = photoPath;
   }
 
   addToContainer(container) {
     const card = document.createElement("div");
     card.classList.add("card");
     container.appendChild(card);
+
+    const foodPhoto = document.createElement("img");
+    foodPhoto.classList.add("food-photo");
+    foodPhoto.src = this.photoPath;
+    card.appendChild(foodPhoto);
 
     const foodName = document.createElement("p");
     foodName.classList.add("food-name");
